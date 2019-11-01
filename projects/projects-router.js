5a8d140c9ccf52e87700ef6467d517ac5426d596
ericbanker12 <ericbanker12@gmail.com>
2019-11-01 11:07:40 -0500

    create middleware validateProjectBody, add POST to create a new project

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const projectModel = require('../data/helpers/projectModel')
 
+// Routing
 router.get('/', (req, res) => {
     projectModel.get()
     .then(resp => {
-        console.log(resp)
+        // console.log(resp)
         res.json(resp)
     })
     .catch(err => {
@@ -12,5 +13,37 @@ router.get('/', (req, res) => {
         res.sendStatus(500)
     })
 })
+
+router.post('/', validateProjectBody, (req, res) => {
+    projectModel.insert(req.body)
+    .then(resp => {
+        // console.log(resp)
+        res.status(201).json(resp)
+    })
+    .catch(err => {
+        console.log(err)
+        res.sendStatus(500)
+    })
+})
+
+// middleware
+function validateProjectBody(req, res, next) {
+    const {name, description, completed} = req.body
+    
+    if (!req.body) {
+        return res.status(400).json({message: 'missing body data'})
+    }
+
+    if (!name || typeof name !== 'string') {
+        return res.status(400).json({message: 'missing required data field name: (String)'})
+    }
+
+    if (!description || typeof description !== 'string') {
+        return res.status(400).json({message: 'missing required data field description: (String)'})
+    }
+
+    req.body = {name, description, completed: completed?true:false}
+    next()
+}
 
 module.exports=router
